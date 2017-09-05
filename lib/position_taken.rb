da7482b06c9acc5862a63edb0d50f26eee036df3
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2017-09-05 17:47:08 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,3 +1,3 @@
 # code your #position_taken? method here!
 def position_taken?(board,index)
-if board[index] == " "|| board[index] == " "|| board[index] == nil return false 
+if board[index] == " "|| board[index] == " "|| board[index] == nil return false
